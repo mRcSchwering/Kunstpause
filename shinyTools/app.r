c71a691010036438cfeb66b73729c58c7b00196a
mRcSchwering <schweringmarc01@gmail.com>
2016-09-22 21:20:42 +0200

    shinyTools ReactiveTextInput
    started on reactive modul for text input with validation

--- shinyTools/app.r
+++ shinyTools/app.r
@@ -3,17 +3,21 @@ library(shinyTools)
 # some function as example
 check <- function(text, add){ if(any(grepl(add$pat, text))) return(paste("Don't use letter", add$pat, "in any entry."))}
 
+source("R/ReactiveTextInput.R")
+
 # little app with module
 ui <- fluidPage(sidebarLayout(
-  sidebarPanel(h2("TextInputUI"),
-               TextInputUI("id1", c("positive Ctrls", "non-targeting Ctrls"), c("positive", "non-targeting"),
-                           help = "use HGNC symbols", horiz = FALSE)
+  sidebarPanel(h2("ReactiveTextInputUI"),
+    numericInput("nGroups", "n Groups", 2, -2, 10),
+    ReactiveTextInputUI("id1", "Groups")
   ),
-  mainPanel(h2("Output of TextInput"), verbatimTextOutput("display"))
+  mainPanel(h2("Output of ReactiveTextInput"), verbatimTextOutput("display"))
 ))
 
+
 server <-function(input, output, session) {
-  display <- callModule(TextInput, "id1", n = 2, checkFun = "check", addArgs = list(pat = "X"))
+  display <- callModule(ReactiveTextInput, "id1", n = reactive(input$nGroups), prefix = NULL, values = c("1", "asd", "asd3"),
+                        checkFun = "check", addArgs = list(pat = "X"))
   output$display <- renderPrint(display())
 }
 
